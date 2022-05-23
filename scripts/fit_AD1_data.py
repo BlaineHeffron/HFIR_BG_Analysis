5dd448eb90c06fe401afa4488c77f3959ee45951
Blaine <baheffron@gmail.com>
2022-05-23 17:28:08 -0400

    add AD1 fit script

---
+++ scripts/fit_AD1_data.py
@@ -0,0 +1,55 @@
+import sys
+import os
+from os.path import dirname, realpath, join
+
+sys.path.insert(1, dirname(dirname(realpath(__file__))))
+
+from src.utilities.util import get_spec_from_root, write_rows_csv
+from argparse import ArgumentParser
+from src.utilities.ROOT_style import ROOT_style
+from src.analysis.AD1_Fitter import fit_spec
+
+en = [5, 7, 9, 10.5, 12]
+
+outdir = join(os.environ["HFIRBG_ANALYSIS"], "AD1_fit")
+
+def main():
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+    arg = ArgumentParser()
+    ROOT_style()
+    arg.add_argument("f", help="path to root file containing AD1 hist", type=str)
+    arg.add_argument("name", help="name for output plots", type=str)
+    args = arg.parse_args()
+    spec = get_spec_from_root(args.f, "SingleSegmentsIoniPlugin/hSumE", "runtime", False)
+    outpath = join(outdir, args.name)
+    if not os.path.exists(outpath):
+        os.mkdir(outpath)
+    labels = ["guas height", "erfc height", "centroid", "std dev", "erfc loc", "A", "B"]
+    mylabels = []
+    for l in labels:
+        mylabels.append(l)
+        mylabels.append(l + " err")
+    mylabels.append("chisqr")
+    mylabels.append("ndf")
+    mydata = []
+    for i in range(len(en) - 1):
+        par, err, cs, ndf = fit_spec(spec, en[i], en[i + 1], outpath)
+        mydata.append([])
+        for p, e in zip(par, err):
+            mydata[-1].append(p)
+            mydata[-1].append(e)
+        mydata[-1].append(cs)
+        mydata[-1].append(ndf)
+        #print("parameters for energy range {0} - {1}".format(en[i], en[i + 1]))
+        #print(par)
+        #print('errors:')
+        #print(err)
+        print("centroid: {0:.3f} ~ {3:.3f}, std dev: {1:.3f} ~ {4:.3f}, erfc location: {2:.3f} ~ {5:.3f}, guass height: {8:.0f}, erfc height: {9:.0f}, chisqr / ndf: {6:.0f} / {7},".format(par[2], par[3], par[4], err[2], err[3], err[4], cs, ndf, par[0], par[1]))
+    outf = join(outpath, "spec_fit.csv")
+    f = open(outf, 'w')
+    write_rows_csv(f, mylabels, mydata, delimiter=',')
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/analysis/AD1_Fitter.py
@@ -0,0 +1,67 @@
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+from copy import copy
+
+from src.analysis.Spectrum import gauss, dgaussdc, dgaussdR, dgaussds
+from scipy.special import erfc
+import numpy as np
+from src.utilities.FitUtils import chisqr
+from src.utilities.PlotUtils import ScatterLinePlot
+
+
+def AD1_gamma_fit(x, Hg, He, c, s, c2, s2, A, B):
+    return gauss(x, Hg, 0, c, s) + He*erfc((x-c2)/s2) + A + B * x
+
+def AD1_gamma_fit_jac(x, Hg, He, c, s, c2, s2, A, B):
+    dHg = gauss(x, 1, 0, c, s)
+    dHe = erfc((x-c2)/s2)
+    dcentroid = dgaussdc(x, Hg, 0, c, s)
+    dsigma = dgaussds(x, Hg, 0, c, s)
+    dc2 = 2*He*np.exp(-((x-c2)/s2)**2)/(np.sqrt(np.pi)*s2)
+    ds2 = 2*He*(x-c2)*np.exp(-((x-c2)/s2)**2)/(s2**2*np.sqrt(np.pi))
+    dA = np.ones(len(x))
+    dB = x
+    return np.hstack((dHg.reshape(-1, 1), dHe.reshape(-1, 1), dcentroid.reshape(-1, 1), dsigma.reshape(-1, 1),
+                      dc2.reshape(-1, 1), ds2.reshape(-1, 1), dA.reshape(-1, 1), dB.reshape(-1, 1)))
+
+
+def fit_spec(spec, e1, e2, plot_dir=""):
+    ind1 = spec.data_at_x(e1)
+    ind2 = spec.data_at_x(e2)
+    xs = spec.get_data_x()
+    if ind2 + 1 > len(xs):
+        xs = xs[ind1:]
+        ys = spec.data[ind1:]
+    else:
+        xs = xs[ind1:ind2+1]
+        ys = spec.data[ind1:ind2+1]
+    sigma = np.sqrt(ys)
+    for i in range(len(sigma)):
+        if sigma[i] == 0:
+            sigma[i] = 1
+    guesses = [np.amax(ys), np.amax(ys)/2, xs[int(len(xs)/2)], (xs[-1]-xs[0])/4, xs[int(len(xs)/2)], (xs[-1] - xs[0])/4, 0, 0]
+    lower_bounds = [0, 0, xs[0], (xs[-1]-xs[0])/20, xs[0], (xs[-1] - xs[0])/20, -np.inf, -np.inf]
+    upper_bounds = [np.amax(ys), np.amax(ys), xs[-1], xs[-1]-xs[0], xs[-1], xs[-1] - xs[0], np.inf, np.inf ]
+    parameters, covariance = curve_fit(AD1_gamma_fit, xs, ys, p0=guesses, sigma=sigma,
+                                       bounds=(lower_bounds, upper_bounds),
+                                       # absolute_sigma=True,  maxfev=100000,
+                                       absolute_sigma=False, jac=AD1_gamma_fit_jac, maxfev=100000)
+    errs = np.sqrt(np.diag(covariance))
+    fity = AD1_gamma_fit(xs, *parameters)
+    cs = chisqr(ys, fity, sigma)
+    linex = np.linspace(xs[0], xs[-1], 100)
+    liney = AD1_gamma_fit(linex, *parameters)
+    jac = AD1_gamma_fit_jac(linex, *parameters)
+    lineerr = np.sqrt(np.diag(np.matmul(jac, np.matmul(covariance, jac.T))))
+    if plot_dir:
+        miny = np.amin(ys)
+        if miny == 0:
+            nonzeros = copy(ys)
+            nonzeros[nonzeros == 0] = 99999999999
+            miny = np.amin(nonzeros)
+        fig = ScatterLinePlot(xs, ys, sigma, linex, liney, lineerr,
+                        ["best fit", r'1 $\sigma$ error', "peak fits to data"], "energy [MeV]",
+                        "counts", ylog=True, legend_loc='best', xmin=xs[0], xmax=xs[-1], ymin=miny)
+        plt.savefig(plot_dir + "/AD1_fit_{0}-{1}.png".format(e1,e2))
+        plt.close()
+    return parameters, errs, cs, len(ys) - 8
