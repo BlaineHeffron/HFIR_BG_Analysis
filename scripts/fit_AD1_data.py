7e7db55b764af6ef002f4f3c31ffa0a59e227f62
Blaine <baheffron@gmail.com>
2022-09-22 13:37:20 -0400

    updated analysis

--- scripts/fit_AD1_data.py
+++ scripts/fit_AD1_data.py
@@ -1,12 +1,13 @@
 import sys
 import os
 from os.path import dirname, realpath, join
+from copy import copy
 
 
 sys.path.insert(1, dirname(dirname(realpath(__file__))))
 
 from src.analysis.Spectrum import SubtractSpectrum
-from src.utilities.FitUtils import minimize_diff
+from src.utilities.FitUtils import minimize_diff, chisqr
 from src.utilities.util import get_spec_from_root, write_rows_csv, set_indices, plot_multi_spectra, get_bins
 from argparse import ArgumentParser
 from src.utilities.ROOT_style import ROOT_style
@@ -15,44 +16,11 @@ from src.analysis.AD1_Fitter import fit_spec
 en = [5, 6.5, 8.5, 10, 12]
 
 outdir = join(os.environ["HFIRBG_ANALYSIS"], "AD1_fit")
-bin_edges = get_bins(0.1, 12, 12*4)
+bin_edges = get_bins(0.1, 14, 14*5)
+range_check = (0.1, 12)
+#range_check = (10., 14.)
 
-def main():
-    if not os.path.exists(outdir):
-        os.mkdir(outdir)
-    arg = ArgumentParser()
-    ROOT_style()
-    arg.add_argument("f", help="path to root file containing AD1 hist", type=str)
-    arg.add_argument("name", help="name for output plots", type=str)
-    arg.add_argument("--path","-p", help="path in root file to spectrum", type=str)
-    arg.add_argument("--bg","-b", help="path to background root file", type=str)
-    arg.add_argument("--px", help="use projection x on data root file", action="store_true")
-    arg.add_argument("--nopluginpath", help="dont use SingleSegmentsIoniPlugin/ as a prefix to path", action="store_true")
-    arg.add_argument("--sim","-s", help="path to sim root file for comparison", type=str)
-    args = arg.parse_args()
-    spec_path = "SingleSegmentsIoniPlugin/hSumE"
-    if args.path:
-        if args.nopluginpath:
-            spec_path = args.path
-        else:
-            spec_path = "SingleSegmentsIoniPlugin/{}".format(args.path)
-    spec = get_spec_from_root(args.f, spec_path, "runtime", False, projectionX=args.px, has_live=not args.nopluginpath)
-    if args.bg:
-        bgspec = get_spec_from_root(args.bg, spec_path, "runtime", False, projectionX=args.px, has_live=not args.nopluginpath)
-        spec = SubtractSpectrum(spec, bgspec)
-    simspec = None
-    if args.sim:
-        simspec = get_spec_from_root(args.sim, "SingleSegmentsIoniPlugin/hSumE", "runtime", False)
-        start_index, end_index = set_indices(0, 0, 0.1, 10, simspec)
-        simspec.rebin(bin_edges)
-        spec.rebin(bin_edges)
-        sim = simspec.get_normalized_hist()[start_index:end_index]
-        data = spec.get_normalized_hist()[start_index:end_index]
-        data_err = spec.get_normalized_err()[start_index:end_index]/100.
-        scale = minimize_diff(sim, data, data_err)
-        simspec.scale_hist(scale, True)
-        print("scale is {}".format(scale))
-    outpath = join(outdir, args.name)
+def get_fits(args, spec, outpath):
     if not os.path.exists(outpath):
         os.mkdir(outpath)
     labels = ["guas height", "erfc height", "centroid", "std dev", "erfc loc", "A", "B"]
@@ -81,11 +49,99 @@ def main():
         outf = join(outpath, "hSumE_fit.csv")
     f = open(outf, 'w')
     write_rows_csv(f, mylabels, mydata, delimiter=',')
-    if simspec is not None:
-        if args.path:
-            plot_multi_spectra({"sim": simspec, "data": spec}, join(outpath, "{}sim_scaled_data_AD1_comparison".format(args.path)), emin=0.1, emax=12, ebars=False)
+
+def main():
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+    arg = ArgumentParser()
+    ROOT_style()
+    arg.add_argument("f", help="path to root file containing AD1 hist", type=str)
+    arg.add_argument("name", help="name for output plots", type=str)
+    arg.add_argument("--path","-p", help="path in root file to spectrum", type=str)
+    arg.add_argument("--bg","-b", help="path to background root file", type=str)
+    arg.add_argument("--px", help="use projection x on data root file", action="store_true")
+    arg.add_argument("--nopluginpath", help="dont use SingleSegmentsIoniPlugin/ as a prefix to path", action="store_true")
+    arg.add_argument("--sim","-s", help="path to sim root file for comparison", type=str)
+    args = arg.parse_args()
+    spec_path = "SingleSegmentsIoniPlugin/hSumE"
+    if args.path:
+        if args.nopluginpath:
+            spec_path = args.path
         else:
-            plot_multi_spectra({"sim": simspec, "data": spec}, join(outpath, "{}_sim_scaled_data_AD1_comparison".format("hSumE")), emin=0.1, emax=12, ebars=False)
+            spec_path = "SingleSegmentsIoniPlugin/{}".format(args.path)
+    spec = get_spec_from_root(args.f, spec_path, "runtime", False, projectionX=args.px, has_live=not args.nopluginpath)
+    if args.bg:
+        bgspec = get_spec_from_root(args.bg, spec_path, "runtime", False, projectionX=args.px, has_live=not args.nopluginpath)
+        spec = SubtractSpectrum(spec, bgspec)
+    simspec = None
+    if args.sim:
+        simspec = get_spec_from_root(args.sim, "SingleSegmentsIoniPlugin/hSumE", "runtime", False)
+        simspec.rebin(bin_edges)
+        spec.rebin(bin_edges)
+        start_index, end_index = set_indices(0, 0, range_check[0], range_check[1], simspec)
+        sim = simspec.get_normalized_hist()[start_index:end_index]
+        data = spec.get_normalized_hist()[start_index:end_index]
+        data_err = spec.get_normalized_err()[start_index:end_index]
+        scale = minimize_diff(sim, data, data_err)
+        simspec.scale_hist(scale, True)
+        chis = chisqr(sim * scale, data, data_err)
+        print("chisqr is {}".format(chis))
+        sim = simspec.get_normalized_hist()[start_index:end_index]
+        chis = chisqr(sim, data, data_err)
+        print("chisqr is {}".format(chis))
+        print("scale is {}".format(scale))
+
+    outpath = join(outdir, args.name)
+    get_fits(args, spec, outpath)
+    if simspec is not None:
+        print("sim fits:")
+        simargs = copy(args)
+        simargs.path = simargs.path + "_sim"
+        get_fits(simargs, simspec, outpath)
+        if args.path:
+            plot_multi_spectra({"sim": simspec, "data": spec}, join(outpath, "{}sim_scaled_data_AD1_comparison".format(args.path)), emin=range_check[0], emax=range_check[1], ebars=False)
+        else:
+            plot_multi_spectra({"sim": simspec, "data": spec}, join(outpath, "{}_sim_scaled_data_AD1_comparison".format("hSumE")), emin=range_check[0], emax=range_check[1], ebars=False)
+
+def check_segs():
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+    arg = ArgumentParser()
+    ROOT_style()
+    arg.add_argument("f", help="path to root file containing AD1 hist", type=str)
+    arg.add_argument("name", help="name for output plots", type=str)
+    arg.add_argument("sim", help="path to sim root file for comparison", type=str)
+    args = arg.parse_args()
+    spec_path = "SingleSegmentsIoniPlugin/hSegEnergy_"
+    simspec = get_spec_from_root(args.sim, "SingleSegmentsIoniPlugin/hSumE", "runtime", False)
+    simspec.rebin(bin_edges)
+    start_index, end_index = set_indices(0, 0, range_check[0], range_check[1], simspec)
+    sim = simspec.get_normalized_hist()[start_index:end_index]
+    chisqrdict = {}
+    for i in range(14*11):
+        try:
+            spec = get_spec_from_root(args.f, spec_path + str(i), "runtime", False)
+            spec.rebin(bin_edges)
+            data = spec.get_normalized_hist()[start_index:end_index]
+            data_err = spec.get_normalized_err()[start_index:end_index]
+            scale = minimize_diff(sim, data, data_err)
+            chis = chisqr(sim * scale, data, data_err)
+            chisqrdict[i] = chis
+            print("seg {}, chisqr {}, scale {}".format(i, chis, scale))
+        except Exception as e:
+            print(e)
+    minchisqr = 1e9
+    minname = ""
+    for name in chisqrdict.keys():
+        if chisqrdict[name] < minchisqr:
+            minchisqr = chisqrdict[name]
+            minname = name
+    print("minimum chisqr is seg {} with chisqr {}".format(minname, minchisqr))
+    for name in chisqrdict.keys():
+        dchisqr = chisqrdict[name] - minchisqr
+        print("delta chisqr seg {} is {}".format(name,dchisqr))
+    print("minimum chisqr is seg {} with chisqr {}".format(minname, minchisqr))
 
 if __name__ == "__main__":
-    main()
+    check_segs()
+    #main()
